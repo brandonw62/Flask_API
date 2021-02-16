50a40a6d4cdb8c50c2e11022c13d58b91bd659f0
Brandon Wood <brandonw62@live.com>
2021-02-16 07:49:00 -0800

    Initial endpoints with tests

---
+++ test.py
@@ -0,0 +1,9 @@
+import requests
+
+BASE ="http://127.0.0.1:5000/"
+
+response = requests.get(BASE + "helloworld")
+print(response.json())
+
+response = requests.post(BASE + "helloworld")
+print(response.json())
